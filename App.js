0cd9f99dad545d84e2504a6043c1c9262e99cf9d
Aditi Saxena <87848601+aditi270800@users.noreply.github.com>
2022-06-13 12:07:40 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+import { useState } from 'react';
+
+function App() {
+
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
